578ad5cc8af3f17cf6d7639c67e98eb63a115945
Christian Kirkegaard <hello@christiank.org>
2011-07-22 19:30:38 +0200

    finished up the lib

--- library/Todo.php
+++ library/Todo.php
@@ -3,36 +3,68 @@
 class Todo
 {
 
-    protected $_options = array();
+    protected $_todos = array();
+
+    protected $_options = array(
+        'file' => '.todo'
+    );
 
     public function __construct($config = array())
     {
-        
+        if(isset($config['file'])) {
+            $this->_options['file'] = $config['file'];
+        }
+        $this->_getFile();
     }
 
     public function add($string, $priority = 0)
     {
-        return 'add item';
+        array_push($this->_todos, array(
+            'todo'     => $string,
+            'priority' => $priority
+        ));
+        return $this->save();
     }
 
     public function delete($id)
     {
-        return 'delete item : ' . $id;
+        unset($this->_todos[$id]);
+        return $this->save();
     }
 
     public function setPriority($id, $priority)
     {
-        
+        $this->_todos[$id]['priority'] = $priority;
+        return $this->save();
     }
 
-    public function getList()
+    public function getTodos($item = null)
     {
-        
+        if(isset($item)) {
+            $list = $this->_todos[$item];
+        } else {
+            $list = $this->_todos;
+        }
+
+        return $this->_sort($list);
+    }
+
+    protected function _sort($list = array())
+    {
+        return $list;
+    }
+
+    protected function _getFile()
+    {
+        $file = unserialize(file_get_contents($this->_options['file']));
+        $this->_todos = !$file ? array() : $file;
+        return $this;
     }
 
     public function save()
     {
-        
+        file_put_contents($this->_options['file'], serialize($this->_todos));
+        return $this;
     }
 
 }
